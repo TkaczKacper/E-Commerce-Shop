f451f085ecc902194ec1a31b95c0c158a07971dc
TkaczKacper <kacpertk2016@gmail.com>
2025-04-04 00:02:42 +0200

    AppDbContext created

--- ShopAPI/ShopAPI/Data/AppDbContext.cs
+++ ShopAPI/ShopAPI/Data/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace ShopAPI.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopAPI.Models;
 
-public class AppDbContext
+namespace ShopAPI.Data;
+
+public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Negotiation> Negotiations { get; set; }
 }
