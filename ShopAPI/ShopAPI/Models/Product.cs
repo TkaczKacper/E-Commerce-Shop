862482c75af6314d9b08d0413dc46edd242c4ee3
TkaczKacper <kacpertk2016@gmail.com>
2025-04-05 01:14:36 +0200

    Product base model

--- ShopAPI/ShopAPI/Models/Product.cs
+++ ShopAPI/ShopAPI/Models/Product.cs
@@ -1,6 +1,15 @@
-﻿namespace ShopAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopAPI.Models;
 
 public class Product
 {
+    [Key]
+    public int Id { get; set; }
     
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    public decimal Price { get; set; }
 }
